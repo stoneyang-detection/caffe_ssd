1d1463c5e0ca03a7aa47eb13023b6670ebfc6144
Wei Liu <lwbiosoft@gmail.com>
2015-04-10 13:14:36 -0700

    simply parse_evaluate_layer and change test accordingly

--- src/caffe/layers/parse_evaluate_layer.cpp
+++ src/caffe/layers/parse_evaluate_layer.cpp
@@ -41,60 +41,30 @@ void ParseEvaluateLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
                                             const vector<Blob<Dtype>*>& top) {
   CHECK_EQ(bottom[0]->num(), bottom[1]->num());
   CHECK_EQ(bottom[0]->count(), bottom[1]->count());
-  const Dtype* bottom_data = bottom[0]->cpu_data();
-  const Dtype* bottom_label = bottom[1]->cpu_data();
+  const Dtype* bottom_pred = bottom[0]->cpu_data();
+  const Dtype* bottom_gt = bottom[1]->cpu_data();
   Dtype* top_data = top[0]->mutable_cpu_data();
   caffe_set(top[0]->count(), Dtype(0), top_data);
   int num = bottom[0]->num();
-  int dim = bottom[0]->count() / bottom[0]->num();
   int spatial_dim = bottom[0]->height() * bottom[0]->width();
   for (int i = 0; i < num; ++i) {
-    // Find all unique labels in the ground truth
-    std::set<Dtype> labels(bottom_label + i * spatial_dim,
-                           bottom_label + (i + 1) * spatial_dim);
-    // Find all unqiue labels in prediction
-    std::set<Dtype> predict_labels(bottom_data + i * spatial_dim,
-                                   bottom_data + (i + 1) * spatial_dim);
-    labels.insert(predict_labels.begin(), predict_labels.end());
-    // Find regions with ignore_labels_
-    Blob<Dtype> ignore_mask(1, 1, bottom[0]->height(), bottom[0]->width());
-    Dtype* ignore_mask_data = ignore_mask.mutable_cpu_data();
-    caffe_set(ignore_mask.count(), Dtype(0), ignore_mask_data);
-    for (typename std::set<Dtype>::iterator it = ignore_labels_.begin();
-         it != ignore_labels_.end(); ++it) {
-      int ignore_label = *it;
-      for (int j = 0; j < spatial_dim; ++j) {
-        if (static_cast<int>(bottom_label[i * spatial_dim + j]) == ignore_label) {
-          ignore_mask_data[j] = 1;
-        }
-      }
-    }
     // count the number of ground truth labels, the predicted labels, and
     // predicted labels happens to be ground truth labels
-    for (typename std::set<Dtype>::iterator it = labels.begin();
-         it != labels.end(); ++it) {
-      int label = *it;
-      int predict_pos_label = 0;
-      int predict_label = 0;
-      int num_pos_label = 0;
-      for (int j = 0; j < spatial_dim; ++j) {
-        if (ignore_mask_data[j] == 1) {
-          continue;
-        }
-        if (static_cast<int>(bottom_label[i * spatial_dim + j]) == label) {
-          ++num_pos_label;
-          if (static_cast<int>(bottom_data[i * dim + j]) == label) {
-            ++predict_pos_label;
-          }
-        }
-        if (static_cast<int>(bottom_data[i * dim + j]) == label) {
-          ++predict_label;
-        }
+    for (int j = 0; j < spatial_dim; ++j) {
+      int gt_label = bottom_gt[i];
+      int pred_label = bottom_pred[i];
+      CHECK_LT(pred_label, num_labels_);
+      if (ignore_labels_.find(gt_label) != ignore_labels_.end()) {
+        continue;
       }
-      top_data[label*3] += predict_pos_label;
-      top_data[label*3 + 1] += num_pos_label;
-      top_data[label*3 + 2] += predict_label;
+      if (gt_label == pred_label) {
+        top_data[gt_label * 3]++;
+      }
+      top_data[gt_label * 3 + 1]++;
+      top_data[pred_label * 3 + 2]++;
     }
+    bottom_pred += bottom[0]->offset(1);
+    bottom_gt += bottom[1]->offset(1);
   }
   // ParseEvaluate layer should not be used as a loss function.
 }
--- src/caffe/test/test_parse_evaluate_layer.cpp
+++ src/caffe/test/test_parse_evaluate_layer.cpp
@@ -21,14 +21,13 @@ class ParseEvaluateLayerTest : public ::testing::Test {
   ParseEvaluateLayerTest()
       : blob_bottom_prediction_(new Blob<Dtype>()),
         blob_bottom_label_(new Blob<Dtype>()),
-        blob_buffer_(new Blob<Dtype>()),
         blob_top_(new Blob<Dtype>()),
-        num_labels_(10) {
+        num_labels_(5) {
     vector<int> shape(4);
     shape[0] = 2;
     shape[1] = 1;
-    shape[2] = 10;
-    shape[3] = 10;
+    shape[2] = 3;
+    shape[3] = 3;
     blob_bottom_prediction_->Reshape(shape);
     blob_bottom_label_->Reshape(shape);
     FillBottoms();
@@ -37,35 +36,77 @@ class ParseEvaluateLayerTest : public ::testing::Test {
     shape[1] = num_labels_;
     shape[2] = 1;
     shape[3] = 3;
-    blob_buffer_->Reshape(shape);
+    blob_top_->Reshape(shape);
 
     blob_bottom_vec_.push_back(blob_bottom_prediction_);
     blob_bottom_vec_.push_back(blob_bottom_label_);
     blob_top_vec_.push_back(blob_top_);
   }
 
-  virtual void FillBottoms() {
-    const unsigned int prefetch_rng_seed = caffe_rng_rand();
-    shared_ptr<Caffe::RNG> rng(new Caffe::RNG(prefetch_rng_seed));
-    caffe::rng_t* prefetch_rng =
-          static_cast<caffe::rng_t*>(rng->generator());
-    Dtype* prediction_data = blob_bottom_prediction_->mutable_cpu_data();
-    Dtype* label_data = blob_bottom_label_->mutable_cpu_data();
-    for (int i = 0; i < blob_bottom_label_->count(); ++i) {
-      prediction_data[i] = (*prefetch_rng)() % num_labels_;
-      label_data[i] = (*prefetch_rng)() % num_labels_;
-    }
-  }
-
   virtual ~ParseEvaluateLayerTest() {
     delete blob_bottom_prediction_;
     delete blob_bottom_label_;
-    delete blob_buffer_;
     delete blob_top_;
   }
+
+  void FillBottoms() {
+    // manually fill some data
+    // Input: (prediction)
+    //     [ 1 1 2 ]
+    //     [ 1 1 2 ]
+    //     [ 1 3 4 ]
+    //     =========
+    //     [ 0 3 2 ]
+    //     [ 3 3 2 ]
+    //     [ 2 0 0 ]
+    blob_bottom_prediction_->mutable_cpu_data()[0] = 1;
+    blob_bottom_prediction_->mutable_cpu_data()[1] = 1;
+    blob_bottom_prediction_->mutable_cpu_data()[2] = 2;
+    blob_bottom_prediction_->mutable_cpu_data()[3] = 1;
+    blob_bottom_prediction_->mutable_cpu_data()[4] = 1;
+    blob_bottom_prediction_->mutable_cpu_data()[5] = 2;
+    blob_bottom_prediction_->mutable_cpu_data()[6] = 1;
+    blob_bottom_prediction_->mutable_cpu_data()[7] = 3;
+    blob_bottom_prediction_->mutable_cpu_data()[8] = 4;
+    blob_bottom_prediction_->mutable_cpu_data()[9] = 0;
+    blob_bottom_prediction_->mutable_cpu_data()[10] = 3;
+    blob_bottom_prediction_->mutable_cpu_data()[11] = 2;
+    blob_bottom_prediction_->mutable_cpu_data()[12] = 3;
+    blob_bottom_prediction_->mutable_cpu_data()[13] = 3;
+    blob_bottom_prediction_->mutable_cpu_data()[14] = 2;
+    blob_bottom_prediction_->mutable_cpu_data()[15] = 2;
+    blob_bottom_prediction_->mutable_cpu_data()[16] = 0;
+    blob_bottom_prediction_->mutable_cpu_data()[17] = 0;
+    // Input: (label)
+    //     [ 1 1 0 ]
+    //     [ 1 0 2 ]
+    //     [ 2 2 0 ]
+    //     =========
+    //     [ 1 2 2 ]
+    //     [ 3 3 2 ]
+    //     [ 0 0 0 ]
+    blob_bottom_label_->mutable_cpu_data()[0] = 1;
+    blob_bottom_label_->mutable_cpu_data()[1] = 1;
+    blob_bottom_label_->mutable_cpu_data()[2] = 0;
+    blob_bottom_label_->mutable_cpu_data()[3] = 1;
+    blob_bottom_label_->mutable_cpu_data()[4] = 0;
+    blob_bottom_label_->mutable_cpu_data()[5] = 2;
+    blob_bottom_label_->mutable_cpu_data()[6] = 2;
+    blob_bottom_label_->mutable_cpu_data()[7] = 2;
+    blob_bottom_label_->mutable_cpu_data()[8] = 0;
+    blob_bottom_label_->mutable_cpu_data()[9] = 1;
+    blob_bottom_label_->mutable_cpu_data()[10] = 2;
+    blob_bottom_label_->mutable_cpu_data()[11] = 2;
+    blob_bottom_label_->mutable_cpu_data()[12] = 3;
+    blob_bottom_label_->mutable_cpu_data()[13] = 3;
+    blob_bottom_label_->mutable_cpu_data()[14] = 2;
+    blob_bottom_label_->mutable_cpu_data()[15] = 0;
+    blob_bottom_label_->mutable_cpu_data()[16] = 0;
+    blob_bottom_label_->mutable_cpu_data()[17] = 0;
+  }
+
   Blob<Dtype>* const blob_bottom_prediction_;
   Blob<Dtype>* const blob_bottom_label_;
-  Blob<Dtype>* const blob_buffer_;
   Blob<Dtype>* const blob_top_;
   vector<Blob<Dtype>*> blob_bottom_vec_;
   vector<Blob<Dtype>*> blob_top_vec_;
@@ -77,11 +118,11 @@ TYPED_TEST_CASE(ParseEvaluateLayerTest, TestDtypes);
 TYPED_TEST(ParseEvaluateLayerTest, TestSetup) {
   LayerParameter layer_param;
   ParseEvaluateParameter* parse_evaluate_param = layer_param.mutable_parse_evaluate_param();
-  parse_evaluate_param->set_num_labels(this->num_labels_);
+  parse_evaluate_param->set_num_labels(10);
   ParseEvaluateLayer<TypeParam> layer(layer_param);
   layer.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
   EXPECT_EQ(this->blob_top_->num(), 1);
-  EXPECT_EQ(this->blob_top_->channels(), this->num_labels_);
+  EXPECT_EQ(this->blob_top_->channels(), 10);
   EXPECT_EQ(this->blob_top_->height(), 1);
   EXPECT_EQ(this->blob_top_->width(), 3);
 }
@@ -90,37 +131,111 @@ TYPED_TEST(ParseEvaluateLayerTest, TestForwardCPU) {
   LayerParameter layer_param;
   Caffe::set_mode(Caffe::CPU);
   ParseEvaluateParameter* parse_evaluate_param = layer_param.mutable_parse_evaluate_param();
-  parse_evaluate_param->set_num_labels(this->num_labels_);
+  parse_evaluate_param->set_num_labels(5);
   ParseEvaluateLayer<TypeParam> layer(layer_param);
   layer.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+  EXPECT_EQ(this->blob_top_->num(), 1);
+  EXPECT_EQ(this->blob_top_->channels(), 5);
+  EXPECT_EQ(this->blob_top_->height(), 1);
+  EXPECT_EQ(this->blob_top_->width(), 3);
   layer.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
 
-  TypeParam predict_label, true_label;
-  TypeParam* buffer_data = this->blob_buffer_->mutable_cpu_data();
-  caffe_set(this->blob_buffer_->count(), TypeParam(0), buffer_data);
-  int index;
-  for (int i = 0; i < 2; ++i) {
-    for (int h = 0; h < 10; ++h) {
-      for (int w = 0; w < 10; ++w) {
-        predict_label = this->blob_bottom_prediction_->data_at(i, 0, h, w);
-        true_label = this->blob_bottom_label_->data_at(i, 0, h, w);
-        if (predict_label == true_label) {
-          index = this->blob_buffer_->offset(0, predict_label, 0, 0);
-          ++buffer_data[index];
-        }
-        index = this->blob_buffer_->offset(0, true_label, 0, 1);
-        ++buffer_data[index];
-        index = this->blob_buffer_->offset(0, predict_label, 0, 2);
-        ++buffer_data[index];
-      }
-    }
-  }
-  for (int c = 0; c < this->num_labels_; ++c) {
-    for (int k = 0; k < 3; ++k) {
-      EXPECT_NEAR(this->blob_top_->data_at(0, c, 0, k),
-                  this->blob_buffer_->data_at(0, c, 0, k), 1e-4);
-    }
-  }
+  // Output:
+  //     [ 2 6 3 ]
+  //     [ 3 4 5 ]
+  //     [ 3 5 5 ]
+  //     [ 2 3 3 ]
+  //     [ 0 0 1 ]
+  this->blob_top_->mutable_cpu_data()[0] = 2;
+  this->blob_top_->mutable_cpu_data()[1] = 6;
+  this->blob_top_->mutable_cpu_data()[2] = 3;
+  this->blob_top_->mutable_cpu_data()[3] = 3;
+  this->blob_top_->mutable_cpu_data()[4] = 4;
+  this->blob_top_->mutable_cpu_data()[5] = 5;
+  this->blob_top_->mutable_cpu_data()[6] = 3;
+  this->blob_top_->mutable_cpu_data()[7] = 5;
+  this->blob_top_->mutable_cpu_data()[8] = 5;
+  this->blob_top_->mutable_cpu_data()[9] = 2;
+  this->blob_top_->mutable_cpu_data()[10] = 3;
+  this->blob_top_->mutable_cpu_data()[11] = 3;
+  this->blob_top_->mutable_cpu_data()[12] = 0;
+  this->blob_top_->mutable_cpu_data()[13] = 0;
+  this->blob_top_->mutable_cpu_data()[14] = 1;
+}
+
+TYPED_TEST(ParseEvaluateLayerTest, TestForwardIgnoreCPU) {
+  LayerParameter layer_param;
+  Caffe::set_mode(Caffe::CPU);
+  ParseEvaluateParameter* parse_evaluate_param = layer_param.mutable_parse_evaluate_param();
+  parse_evaluate_param->set_num_labels(5);
+  parse_evaluate_param->add_ignore_label(0);
+  ParseEvaluateLayer<TypeParam> layer(layer_param);
+  layer.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+  EXPECT_EQ(this->blob_top_->num(), 1);
+  EXPECT_EQ(this->blob_top_->channels(), 5);
+  EXPECT_EQ(this->blob_top_->height(), 1);
+  EXPECT_EQ(this->blob_top_->width(), 3);
+  layer.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+
+  // Output:
+  //     [ 0 0 0 ]
+  //     [ 3 4 5 ]
+  //     [ 3 5 5 ]
+  //     [ 2 3 3 ]
+  //     [ 0 0 1 ]
+  this->blob_top_->mutable_cpu_data()[0] = 0;
+  this->blob_top_->mutable_cpu_data()[1] = 0;
+  this->blob_top_->mutable_cpu_data()[2] = 0;
+  this->blob_top_->mutable_cpu_data()[3] = 3;
+  this->blob_top_->mutable_cpu_data()[4] = 4;
+  this->blob_top_->mutable_cpu_data()[5] = 5;
+  this->blob_top_->mutable_cpu_data()[6] = 3;
+  this->blob_top_->mutable_cpu_data()[7] = 5;
+  this->blob_top_->mutable_cpu_data()[8] = 5;
+  this->blob_top_->mutable_cpu_data()[9] = 2;
+  this->blob_top_->mutable_cpu_data()[10] = 3;
+  this->blob_top_->mutable_cpu_data()[11] = 3;
+  this->blob_top_->mutable_cpu_data()[12] = 0;
+  this->blob_top_->mutable_cpu_data()[13] = 0;
+  this->blob_top_->mutable_cpu_data()[14] = 1;
+}
+
+TYPED_TEST(ParseEvaluateLayerTest, TestForwardIgnoreTwoCPU) {
+  LayerParameter layer_param;
+  Caffe::set_mode(Caffe::CPU);
+  ParseEvaluateParameter* parse_evaluate_param = layer_param.mutable_parse_evaluate_param();
+  parse_evaluate_param->set_num_labels(5);
+  parse_evaluate_param->add_ignore_label(0);
+  parse_evaluate_param->add_ignore_label(4);
+  ParseEvaluateLayer<TypeParam> layer(layer_param);
+  layer.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+  EXPECT_EQ(this->blob_top_->num(), 1);
+  EXPECT_EQ(this->blob_top_->channels(), 5);
+  EXPECT_EQ(this->blob_top_->height(), 1);
+  EXPECT_EQ(this->blob_top_->width(), 3);
+  layer.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+
+  // Output:
+  //     [ 0 0 0 ]
+  //     [ 3 4 5 ]
+  //     [ 3 5 5 ]
+  //     [ 2 3 3 ]
+  //     [ 0 0 0 ]
+  this->blob_top_->mutable_cpu_data()[0] = 0;
+  this->blob_top_->mutable_cpu_data()[1] = 0;
+  this->blob_top_->mutable_cpu_data()[2] = 0;
+  this->blob_top_->mutable_cpu_data()[3] = 3;
+  this->blob_top_->mutable_cpu_data()[4] = 4;
+  this->blob_top_->mutable_cpu_data()[5] = 5;
+  this->blob_top_->mutable_cpu_data()[6] = 3;
+  this->blob_top_->mutable_cpu_data()[7] = 5;
+  this->blob_top_->mutable_cpu_data()[8] = 5;
+  this->blob_top_->mutable_cpu_data()[9] = 2;
+  this->blob_top_->mutable_cpu_data()[10] = 3;
+  this->blob_top_->mutable_cpu_data()[11] = 3;
+  this->blob_top_->mutable_cpu_data()[12] = 0;
+  this->blob_top_->mutable_cpu_data()[13] = 0;
+  this->blob_top_->mutable_cpu_data()[14] = 0;
 }
 
 }  // namespace caffe
