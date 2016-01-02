36001c28da2ff31cae2bf8d5b47188e5577b49df
Wei Liu <lwbiosoft@gmail.com>
2016-01-02 13:28:06 -0500

    fix bugs and add test for multiple input image case in test_parse_output_layer

--- src/caffe/test/test_parse_output_layer.cpp
+++ src/caffe/test/test_parse_output_layer.cpp
@@ -20,17 +20,27 @@ class ParseOutputLayerTest : public ::testing::Test {
  protected:
   ParseOutputLayerTest()
       : blob_bottom_(new Blob<Dtype>()),
+        blob_bottom2_(new Blob<Dtype>()),
         blob_top_label_(new Blob<Dtype>()),
         blob_top_prob_(new Blob<Dtype>()) {
     blob_bottom_->Reshape(1, 2, 3, 3);
     FillBottoms();
 
+    multi_num_ = 4;
+    blob_bottom2_->Reshape(multi_num_, 2, 3, 3);
+    for (int i = 0; i < multi_num_; ++i) {
+      caffe_copy(blob_bottom_->count(), blob_bottom_->cpu_data(),
+                 blob_bottom2_->mutable_cpu_data() + i * blob_bottom_->count());
+    }
+
     blob_bottom_vec_.push_back(blob_bottom_);
+    blob_bottom_multi_vec_.push_back(blob_bottom2_);
     blob_top_vec_.push_back(blob_top_label_);
   }
 
   virtual ~ParseOutputLayerTest() {
     delete blob_bottom_;
+    delete blob_bottom2_;
     delete blob_top_label_;
     delete blob_top_prob_;
   }
@@ -66,10 +76,14 @@ class ParseOutputLayerTest : public ::testing::Test {
   }
 
   Blob<Dtype>* const blob_bottom_;
+  Blob<Dtype>* const blob_bottom2_;
   Blob<Dtype>* const blob_top_label_;
   Blob<Dtype>* const blob_top_prob_;
   vector<Blob<Dtype>*> blob_bottom_vec_;
+  vector<Blob<Dtype>*> blob_bottom_multi_vec_;
   vector<Blob<Dtype>*> blob_top_vec_;
+
+  int multi_num_;
 };
 
 TYPED_TEST_CASE(ParseOutputLayerTest, TestDtypes);
@@ -112,18 +126,46 @@ TYPED_TEST(ParseOutputLayerTest, TestForward) {
   layer.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
 
   // Output:
-  //     [ 0 1 1 ]
-  //     [ 1 1 1 ]
+  //     [ 0 1 0 ]
+  //     [ 1 1 0 ]
   //     [ 1 0 0 ]
-  this->blob_top_label_->mutable_cpu_data()[0] = 0;
-  this->blob_top_label_->mutable_cpu_data()[1] = 1;
-  this->blob_top_label_->mutable_cpu_data()[2] = 1;
-  this->blob_top_label_->mutable_cpu_data()[3] = 1;
-  this->blob_top_label_->mutable_cpu_data()[4] = 1;
-  this->blob_top_label_->mutable_cpu_data()[5] = 1;
-  this->blob_top_label_->mutable_cpu_data()[6] = 1;
-  this->blob_top_label_->mutable_cpu_data()[7] = 0;
-  this->blob_top_label_->mutable_cpu_data()[8] = 0;
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[0], 0);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[1], 1);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[2], 0);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[3], 1);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[4], 1);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[5], 0);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[6], 1);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[7], 0);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[8], 0);
+}
+
+TYPED_TEST(ParseOutputLayerTest, TestMultiForward) {
+  Caffe::set_mode(Caffe::CPU);
+  LayerParameter layer_param;
+  ParseOutputLayer<TypeParam> layer(layer_param);
+  layer.SetUp(this->blob_bottom_multi_vec_, this->blob_top_vec_);
+  EXPECT_EQ(this->blob_top_label_->num(), this->multi_num_);
+  EXPECT_EQ(this->blob_top_label_->channels(), 1);
+  EXPECT_EQ(this->blob_top_label_->height(), 3);
+  EXPECT_EQ(this->blob_top_label_->width(), 3);
+  layer.Forward(this->blob_bottom_multi_vec_, this->blob_top_vec_);
+
+  // Output:
+  //     [ 0 1 0 ]
+  //     [ 1 1 0 ]
+  //     [ 1 0 0 ]
+  for (int i = 0; i < this->multi_num_; ++i) {
+    EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[0 + i*9], 0);
+    EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[1 + i*9], 1);
+    EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[2 + i*9], 0);
+    EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[3 + i*9], 1);
+    EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[4 + i*9], 1);
+    EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[5 + i*9], 0);
+    EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[6 + i*9], 1);
+    EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[7 + i*9], 0);
+    EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[8 + i*9], 0);
+  }
 }
 
 TYPED_TEST(ParseOutputLayerTest, TestForwardTwo) {
@@ -143,32 +185,32 @@ TYPED_TEST(ParseOutputLayerTest, TestForwardTwo) {
   layer.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
 
   // Output: (label)
-  //     [ 0 1 1 ]
-  //     [ 1 1 1 ]
+  //     [ 0 1 0 ]
+  //     [ 1 1 0 ]
   //     [ 1 0 0 ]
-  this->blob_top_label_->mutable_cpu_data()[0] = 0;
-  this->blob_top_label_->mutable_cpu_data()[1] = 1;
-  this->blob_top_label_->mutable_cpu_data()[2] = 1;
-  this->blob_top_label_->mutable_cpu_data()[3] = 1;
-  this->blob_top_label_->mutable_cpu_data()[4] = 1;
-  this->blob_top_label_->mutable_cpu_data()[5] = 1;
-  this->blob_top_label_->mutable_cpu_data()[6] = 1;
-  this->blob_top_label_->mutable_cpu_data()[7] = 0;
-  this->blob_top_label_->mutable_cpu_data()[8] = 0;
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[0], 0);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[1], 1);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[2], 0);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[3], 1);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[4], 1);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[5], 0);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[6], 1);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[7], 0);
+  EXPECT_EQ(this->blob_top_label_->mutable_cpu_data()[8], 0);
 
   // Output: (prob)
   //     [ 1 3 2 ]
   //     [ 3 3 2 ]
   //     [ 2 3.5 4]
-  this->blob_top_prob_->mutable_cpu_data()[0] = 1;
-  this->blob_top_prob_->mutable_cpu_data()[1] = 3;
-  this->blob_top_prob_->mutable_cpu_data()[2] = 2;
-  this->blob_top_prob_->mutable_cpu_data()[3] = 3;
-  this->blob_top_prob_->mutable_cpu_data()[4] = 3;
-  this->blob_top_prob_->mutable_cpu_data()[5] = 2;
-  this->blob_top_prob_->mutable_cpu_data()[6] = 2;
-  this->blob_top_prob_->mutable_cpu_data()[7] = 3.5;
-  this->blob_top_prob_->mutable_cpu_data()[8] = 4;
+  EXPECT_EQ(this->blob_top_prob_->mutable_cpu_data()[0], 1);
+  EXPECT_EQ(this->blob_top_prob_->mutable_cpu_data()[1], 3);
+  EXPECT_EQ(this->blob_top_prob_->mutable_cpu_data()[2], 2);
+  EXPECT_EQ(this->blob_top_prob_->mutable_cpu_data()[3], 3);
+  EXPECT_EQ(this->blob_top_prob_->mutable_cpu_data()[4], 3);
+  EXPECT_EQ(this->blob_top_prob_->mutable_cpu_data()[5], 2);
+  EXPECT_EQ(this->blob_top_prob_->mutable_cpu_data()[6], 2);
+  EXPECT_EQ(this->blob_top_prob_->mutable_cpu_data()[7], 3.5);
+  EXPECT_EQ(this->blob_top_prob_->mutable_cpu_data()[8], 4);
 }
 
 }  // namespace caffe
